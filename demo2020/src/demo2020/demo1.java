f485db3b083bf240de348a8ac159ff94de11461c
8-孙锦镖-034 <44825509+sunjinbiao123@users.noreply.github.com>
2020-11-25 09:39:26 +0800

    Add files via upload

---
+++ demo2020/src/demo2020/demo1.java
@@ -0,0 +1,13 @@
+package demo2020;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class demo1 {
+	@Test
+	public void test1() {
+		assertEquals(1, 1);
+	}
+
+}
